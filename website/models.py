8338689ffeba66f1f729d42ffbbf308dc7f21db2
Ajay <s.ajaykartheek@gmail.com>
2024-03-10 14:42:09 +0530

    update

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	PersonID = models.CharField(max_length=50)
+	first_name = models.CharField(max_length=50)
+	last_name =  models.CharField(max_length=50)
+	TFN =  models.CharField(max_length=50)
+	DOB = models.DateField(max_length=15)
+	address =  models.CharField(max_length=100)
+	SelectEmpyeeToCheckSS =  models.CharField(max_length=50)
+	declarationAcceptance =  models.CharField(max_length=50)
+	
+
+	def __str__(self):
+		return(f"{self.first_name} {self.last_name}")
--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Person
+
 
 def home(request):
+    records = Person.objects.all()
+
+
     #check to see if logging in 
     if request.method == "POST":
         username = request.POST['username']
@@ -17,7 +23,7 @@ def home(request):
             messages.success(request, "There Was An Error Logging In , Please Try Again...")
             return redirect('home')
     else:   
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
 
 def login_user(request):
     pass
@@ -28,5 +34,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request,'register.html',{})
-    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username =  form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You have been successfully registerd! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request,'register.html',{'form':form})
+        
+    return render(request,'register.html',{'form':form})
+
+
